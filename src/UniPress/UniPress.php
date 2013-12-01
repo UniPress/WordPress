381b443dab8d0501ccd0ee38ff1dc91e18fc8481
Jérémy LEHERPEUR <jeremy@leherpeur.net>
2013-12-01 21:08:38 +0100

    Initiate Container

--- src/UniPress/UniPress.php
+++ src/UniPress/UniPress.php
@@ -2,6 +2,19 @@
 
 namespace UniPress;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 class UniPress
 {
+    /** @var ContainerBuilder */
+    private static $container;
+
+    public static function getContainer()
+    {
+        if (!self::$container) {
+            self::$container = new ContainerBuilder();
+        }
+
+        return self::$container;
+    }
 }
